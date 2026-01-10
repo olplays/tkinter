171cedc8c61ad5f5cb3b6fb20fe01a007fb00e2e
olplays <150514182+olplays@users.noreply.github.com>
2026-01-10 12:00:20 +0000

    Add files via upload

--- project11.py
+++ project11.py
@@ -1,17 +1,57 @@
 import pyautogui
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
 W,H=pyautogui.size()
 print(pyautogui.size())
 root=Tk()
 root.geometry(f"{W}x{H}")
 root.title("pizza delivery app")
 root.config(background="red")
-Label(root,text="pizza delivery",background="blue",foreground="red",borderwidth=5,relief="raised",font="roboto 20 bold",anchor="n",).grid(row=0,column=3)
-Label(root,text="SELECT YOUR PIZZA",background="yellow", font="roboto 20").grid(row=1,column=2)
+Label(root,text="pizza delivery",background="blue",foreground="red",borderwidth=5,relief="raised",font="roboto 20 bold",anchor="n",).grid(row=0,column=3, pady=20)
+Label(root,text="SELECT YOUR PIZZA",background="yellow", font="roboto 20").grid(row=1,column=2,padx=20,pady=20)
+
 pizza_types=["cheese","pepperoni","vegetable","tandori"]
-pizza_combo=ttk.Combobox(root,text="select your pizza",values=pizza_types)
-pizza_combo.grid(row=2,column=2)
-Radiobutton(root,text="LARGE",background="pink").grid(row=3,column=2)
-Checkbutton(root,text="").grid(row=3,column=4)
+pizza_var=StringVar()
+pizza_combo=ttk.Combobox(root,textvariable=pizza_var,values=pizza_types,font="roboto 20")
+pizza_combo.grid(row=1,column=3,padx=20,pady=20)
+pizza_combo.current(0)
+size_label=Label(root,text="SELECT YOUR SIZE",background="yellow", font="roboto 20").grid(row=2,column=1,padx=20,pady=20)
+
+size=StringVar(value="small")
+Radiobutton(root,text="LARGE",background="pink", font="roboto 15",variable=size,value="large").grid(row=3,column=1,padx=20,pady=20)
+Radiobutton(root,text="MEDIUM",bg="red", font="roboto 15",variable=size,value="medium").grid(row=4,column=1,padx=20,pady=20)
+Radiobutton(root,text="SMALL",bg="yellow", font="roboto 15",variable=size,value="small").grid(row=5,column=1,padx=20,pady=20)
+
+Label(root,text="SELECT YOUR TOPPINGS",background="yellow", font="roboto 20").grid(row=2,column=4,padx=20,pady=20)
+
+pepperoni=BooleanVar()
+mushrooms=BooleanVar()
+onions=BooleanVar()
+
+
+Checkbutton(root,text="pepperoni", font="roboto 15",variable=pepperoni).grid(row=3,column=4,padx=20,pady=20)
+Checkbutton(root,text="mushrooms", font="roboto 15",variable=mushrooms).grid(row=4,column=4,padx=20,pady=20)
+Checkbutton(root,text="onions", font="roboto 15",variable=onions).grid(row=5,column=4,padx=20,pady=20)
+
+
+spin=Spinbox(root, font="roboto 15",from_=1,to=10)
+spin.grid(row=7,column=3,pady=20)
+
+Label(root,text="QUANTITY OF PIZZAS",font="roboto 20").grid(row=6,column=3,pady=20)
+
+def order():
+    pizza=pizza_var.get()
+    quantity=spin.get()
+    s=size.get()
+    toppings=[]
+    if pepperoni.get(): toppings.append("pepperoni")
+    if mushrooms.get(): toppings.append("mushrooms")
+    if onions.get: toppings.append("onions")
+    text=" , ".join(toppings) if toppings else "no extra toppings selected"    
+    print(f"{pizza} , {quantity} , {s} , {text}")
+Button(root,text="ORDER",font="roboto 30",background="blue",foreground="red",width=15,height=5, command=order()).grid(row=8,column=3)
+
+
+
 root.mainloop()
