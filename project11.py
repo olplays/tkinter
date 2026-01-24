1b94966b8550e8230ccea4bac9433efa982eb8cf
olplays <150514182+olplays@users.noreply.github.com>
2026-01-24 11:51:56 +0000

    Add files via upload

--- project11.py
+++ project11.py
@@ -40,17 +40,29 @@ spin.grid(row=7,column=3,pady=20)
 
 Label(root,text="QUANTITY OF PIZZAS",font="roboto 20").grid(row=6,column=3,pady=20)
 
+Label(root,text="SELECT DRINKS",background="yellow", font="roboto 20").grid(row=1,column=5,padx=20,pady=20)
+drinks=["Coke","Pepsi","Fanta","Sprite"]
+drink_var=StringVar()
+drink_combo=ttk.Combobox(root,textvariable=drink_var,values=drinks,font="roboto 20")
+drink_combo.grid(row=1,column=6,padx=20,pady=20)
+drink_combo.current(0)
+
+
+
 def order():
     pizza=pizza_var.get()
     quantity=spin.get()
     s=size.get()
     toppings=[]
+    drink=drink_var.get()
     if pepperoni.get(): toppings.append("pepperoni")
     if mushrooms.get(): toppings.append("mushrooms")
     if onions.get: toppings.append("onions")
     text=" , ".join(toppings) if toppings else "no extra toppings selected"    
-    print(f"{pizza} , {quantity} , {s} , {text}")
-Button(root,text="ORDER",font="roboto 30",background="blue",foreground="red",width=15,height=5, command=order()).grid(row=8,column=3)
+    message = f"{quantity} {s} {pizza} pizza(s) topping(S): {text}, drink: {drink}."
+
+    messagebox.showinfo("order placed",message)
+Button(root,text="ORDER",font="roboto 30",background="blue",foreground="red",width=15,height=5, command=order).grid(row=8,column=3)
 
 
 
